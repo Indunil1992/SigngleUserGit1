33d5ad9d812fd38c3b51ec5af68bf22ae701fb52
Indunil1992 <indunil@adroitlogic.com>
2019-11-01 11:24:09 +0530

    check con issue

--- SigngleUserGit1/nerJSFunFile.js
+++ SigngleUserGit1/nerJSFunFile.js
@@ -1,4 +1,4 @@
 exports.handler = function(event, context, callback) {
-    // new js concurrency  180
+    // new js concurrency  180 con
     callback(null, {"message": "Successfully executed"});
 }
